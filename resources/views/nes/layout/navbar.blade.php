ec75fd8b777e6d030b9f1bdc96339b07f5fa59b6
biiiboy <jhonmark.delrosario1@gmail.com>
2022-03-23 10:41:51 +0800

    creating dev branch

--- resources/views/nes/layout/navbar.blade.php
+++ resources/views/nes/layout/navbar.blade.php
@@ -15,16 +15,18 @@
     <link rel="stylesheet" href="{{ asset('assets/css/slicknav.min.css')}}">
     <!-- amchart css -->
     <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
-    <!-- others css -->
-    <link rel="stylesheet" href="{{ asset('assets/css/typography.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/css/default-css.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/css/styles.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css')}}">
+    
     <!-- Start datatable css -->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.jqueryui.min.css">
+
+    <!-- others css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/typography.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/default-css.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css')}}">
     
     <!-- modernizr css -->
     <script src="{{ asset('assets/js/vendor/modernizr-2.8.3.min.js')}}"></script>
@@ -474,15 +476,8 @@
         </div>
     </div>
     <!-- offset area end -->
-    <!-- jquery latest version -->
-    <script src="{{ asset('assets/js/vendor/jquery-2.2.4.min.js')}}"></script>
-    <!-- bootstrap 4 js -->
-    <script src="{{ asset('assets/js/popper.min.js')}}"></script>
-    <script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
-    <script src="{{ asset('assets/js/owl.carousel.min.js')}}"></script>
-    <script src="{{ asset('assets/js/metisMenu.min.js')}}"></script>
-    <script src="{{ asset('assets/js/jquery.slimscroll.min.js')}}"></script>
-    <script src="{{ asset('assets/js/jquery.slicknav.min.js')}}"></script>
+    
+    
 
     <!-- start chart js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
@@ -498,6 +493,15 @@
     <script src="{{ asset('assets/js/line-chart.js')}}"></script>
     <!-- all pie chart -->
     <script src="{{ asset('assets/js/pie-chart.js')}}"></script>
+    <!-- jquery latest version -->
+    <script src="{{ asset('assets/js/vendor/jquery-2.2.4.min.js')}}"></script>
+    <!-- bootstrap 4 js -->
+    <script src="{{ asset('assets/js/popper.min.js')}}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{ asset('assets/js/owl.carousel.min.js')}}"></script>
+    <script src="{{ asset('assets/js/metisMenu.min.js')}}"></script>
+    <script src="{{ asset('assets/js/jquery.slimscroll.min.js')}}"></script>
+    <script src="{{ asset('assets/js/jquery.slicknav.min.js')}}"></script>
     <!-- Start datatable js -->
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
     <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
