c68beecb632a87316d14844a962151def7dc3ad1
biiiboy <jhonmark.delrosario1@gmail.com>
2022-03-23 16:03:11 +0800

    adding dashboard branch

--- resources/views/nes/dashboard.blade.php
+++ resources/views/nes/dashboard.blade.php
@@ -2,6 +2,33 @@
 
 @section('content')
 <div class="main-content-inner">
-    
+<div class="row">
+        <div class="col-lg-8">
+            <div class="row">
+                <div class="col-md-6 mt-5 mb-3">
+                    <div class="card">
+                        <div class="seo-fact sbg1">
+                            <div class="p-4 d-flex justify-content-between align-items-center">
+                                <div class="seofct-icon"><i class="ti-user"></i> Students</div>
+                                <h2>2,315</h2>
+                            </div>
+                            <canvas id="seolinechart1" height="50"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 mt-md-5 mb-3">
+                    <div class="card">
+                        <div class="seo-fact sbg2">
+                            <div class="p-4 d-flex justify-content-between align-items-center">
+                                <div class="seofct-icon"><i class="ti-book"></i> Courses</div>
+                                <h2>3,984</h2>
+                            </div>
+                            <canvas id="seolinechart2" height="50"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection()
