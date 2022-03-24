f903583a6fa9fd3738d1d5699d84dd88aaff0094
unknown <fernanda.cartujano@gmail.com>
2022-03-24 10:46:03 +0800

    update students table

--- resources/views/nes/student_list.blade.php
+++ resources/views/nes/student_list.blade.php
@@ -3,30 +3,26 @@
 @section('content')
 <div class="main-content-inner">
     <div class="row">
-     <!-- basic table start -->
-        <div class="col-lg-10 mt-5">
+    <!-- data table start -->
+        <div class="col-12 mt-5">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="header-title">Student List </h4>
-                        <div class="single-table">
-                            <div class="table-responsive">
-                                <table class="table text-center">
-                                    <thead class="text-uppercase">
-                                        <tr>
-                                            <th scope="col">ID</th>
-                                            <th scope="col">First Name</th>
-                                            <th scope="col">Middle Name</th>
-                                            <th scope="col">Last Name</th>
-                                            <th scope="col">Student No.</th>
-                                            <th scope="col">Course</th>
-                                            <th scope="col">Year</th>
-                                            <th scope="col">Section</th>
-                                            <th scope="col">action</th>
-                                        </tr>
-                                    </thead>
-                                </table>
-                            </div>
-                        </div>
+                    <h4 class="header-title">Data Table Default</h4>
+                    <div class="data-tables">
+                        <table id="dataTable" class="text-center">
+                            <thead class="bg-light text-capitalize">
+                                <tr>
+                                    <th>Id</th>
+                                    <th>First Name</th>
+                                    <th>Middle Name</th>
+                                    <th>Last Name</th>
+                                    <th>Student No.</th>
+                                    <th>Course</th>
+                                    <th>Year</th>
+                                    <th>Section</th>
+                                </tr>
+                            </thead>
+                        </table>
                     </div>
                 </div>
             </div>
