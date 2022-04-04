84af7470762988bf5033d5ddca88dfc566208561
biiiboy <jhonmark.delrosario1@gmail.com>
2022-04-04 16:29:53 +0800

    tab design

--- resources/views/nes/change-password.blade.php
+++ resources/views/nes/change-password.blade.php
@@ -8,22 +8,20 @@
             <div class="card-body">
                 <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link active" id="pills-password-tab" data-toggle="pill" href="#pills-password" role="tab" aria-controls="pills-password" aria-selected="false">Password</a>
+                        <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true"><i class="ti-info"></i> Contact</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</a>
+                        <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false"><i class="ti-lock"></i> Password</a>
                     </li>
+                    
                 </ul>
                 <div class="tab-content" id="pills-tabContent">
-                    <div class="tab-pane fade show-active" id="pills-password" role="tabpanel" aria-labelledby="pills-password-tab">
-                    <div class="row justify-content-center">
-                        <div class="col-md-8">
-                            <div class="card">
-                                <div class="card-header">{{ __('Change Password') }}</div>
-
+                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                        <h5 class="mt-4">{{ __('Change Contact') }}</h5>
+                        <div class="row justify-content-center mt-4">
+                            <div class="col-md-8">
                                 <form action="{{ route('update-password') }}" method="POST">
                                     @csrf
-                                    <div class="card-body">
                                         @if (session('status'))
                                             <div class="alert alert-success" role="alert">
                                                 {{ session('status') }}
@@ -32,7 +30,7 @@
                                             <div class="alert alert-danger" role="alert">
                                                 {{ session('error') }}
                                             </div>
-                                         @endif
+                                            @endif
 
                                         <div class="mb-3">
                                             <label for="oldPasswordInput" class="form-label">Old Password</label>
@@ -40,7 +38,7 @@
                                         @error('old_password')
                                             <span class="text-danger">{{ $message }}</span>
                                         @enderror
-                                    </div>
+                                        </div>
                                     <div class="mb-3">
                                         <label for="newPasswordInput" class="form-label">New Password</label>
                                         <input name="new_password" type="password" class="form-control @error('new_password') is-invalid @enderror" id="newPasswordInput"
@@ -55,26 +53,22 @@
                                             placeholder="Confirm New Password">
                                     </div>
 
-                                </div>
+                                
 
-                                <div class="card-footer">
+                                
                                     <button class="btn btn-success">Submit</button>
-                                </div>
-
-                            </form>
+                                    
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-                    </div>
-                    <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
-                    <div class="row justify-content-center">
-                        <div class="col-md-8">
-                            <div class="card">
-                                <div class="card-header">{{ __('Change Password') }}</div>
+                    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                        <h5 class="mt-4">{{ __('Change Password') }}</h5>
 
+                        <div class="row justify-content-center mt-4">
+                            <div class="col-md-8">
                                 <form action="{{ route('update-password') }}" method="POST">
                                     @csrf
-                                    <div class="card-body">
                                         @if (session('status'))
                                             <div class="alert alert-success" role="alert">
                                                 {{ session('status') }}
@@ -83,7 +77,7 @@
                                             <div class="alert alert-danger" role="alert">
                                                 {{ session('error') }}
                                             </div>
-                                         @endif
+                                            @endif
 
                                         <div class="mb-3">
                                             <label for="oldPasswordInput" class="form-label">Old Password</label>
@@ -91,7 +85,7 @@
                                         @error('old_password')
                                             <span class="text-danger">{{ $message }}</span>
                                         @enderror
-                                    </div>
+                                        </div>
                                     <div class="mb-3">
                                         <label for="newPasswordInput" class="form-label">New Password</label>
                                         <input name="new_password" type="password" class="form-control @error('new_password') is-invalid @enderror" id="newPasswordInput"
@@ -106,19 +100,20 @@
                                             placeholder="Confirm New Password">
                                     </div>
 
-                                </div>
+                                
 
-                                <div class="card-footer">
+                                
                                     <button class="btn btn-success">Submit</button>
-                                </div>
-
-                            </form>
+                                    
+                                </form>
+                            </div>
                         </div>
                     </div>
-                    </div>
+                    
                 </div>
             </div>
         </div>
     </div>
+    
 </div>
 @endsection()
