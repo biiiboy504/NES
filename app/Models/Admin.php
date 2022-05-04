b0c28fbab3d2cfffebcea650b64cdd690cf4da53
AustinJayC <acusay12@gmail.com>
2022-05-05 02:26:26 +0800

    a

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
 
+
 class Admin extends Model implements Auditable
 {
     use HasFactory;
