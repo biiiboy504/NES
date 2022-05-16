a42c42004e5d1588e6bcefb03e0aec7119512834
AustinJayC <acusay12@gmail.com>
2022-05-16 13:03:38 +0800

    log table

--- resources/views/nes/logs.blade.php
+++ resources/views/nes/logs.blade.php
@@ -2,6 +2,49 @@
 
 @section('content')
 <div class="main-content-inner">
-    
+    <div class="row">
+        <!-- data table start -->
+        <div class="col-12 mt-5">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="float-left">
+                                <h4 class="header-title">Activity Log</h4>    
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                    <table id="dataTable1" class="table table-bordered table-hover text-center">
+                        <thead class="bg-light text-capitalize">
+                            <tr>
+                                <th>Id</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Event</th>
+                                <th>IP Address</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+@endsection()
+
+@section('scripts')
+<script>
+    $(function () {
+        $("#dataTable1").DataTable({
+            "responsive": true, 
+            "lengthChange": false, 
+            "autoWidth": false,
+            
+            "buttons": ["csv", "pdf", "print", "colvis"],
+            "bDestroy": true
+        }).buttons().container().appendTo('#dataTable1_wrapper .col-md-6:eq(0)');        
+    });
+</script>
 @endsection()
