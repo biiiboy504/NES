322d153b5832456623df223c2a738d5007190ac1
biiiboy <jhonmark.delrosario1@gmail.com>
2022-05-19 11:43:49 +0800

    dashboard dynamic design

--- resources/views/nes/dashboard.blade.php
+++ resources/views/nes/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="main-content-inner">
-<div class="row">
+    <!-- <div class="row">
         <div class="col-lg-12">
             <div class="row">
                 <div class="col-md-4 mt-5 mb-3">
@@ -104,6 +104,28 @@
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div> -->
+
+    <!-- Design Loop -->
+    <div class="row">
+        @foreach ($courses as $course)
+        <div class="col-md-4 mt-5 mb-3">
+            <div class="card">
+                <div class="seo-fact sbg1">
+                    <div class="p-4 d-flex justify-content-between align-items-center">
+                        <div class="seofct-icon"><i class="ti-user"></i>{{$course->course_name}}</div>
+                        <h2>{{$studentCount}}</h2>
+                    </div>
+                    <canvas id="seolinechart1" height="50"></canvas>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+                
             
             
             
