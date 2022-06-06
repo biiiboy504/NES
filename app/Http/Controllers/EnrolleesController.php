251545bb9085d87a757986aec7dafebd5b86c29b
biiiboy <jhonmark.delrosario1@gmail.com>
2022-06-06 09:19:37 +0800

    adding enrollee table

---
+++ app/Http/Controllers/EnrolleesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Enrollees;
+use Illuminate\Http\Request;
+
+class EnrolleesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Enrollees  $enrollees
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Enrollees $enrollees)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Enrollees  $enrollees
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Enrollees $enrollees)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Enrollees  $enrollees
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Enrollees $enrollees)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Enrollees  $enrollees
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Enrollees $enrollees)
+    {
+        //
+    }
+}
